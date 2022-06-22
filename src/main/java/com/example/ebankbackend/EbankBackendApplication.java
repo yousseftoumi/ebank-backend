22c927b26ad929dc812317d6f3775e95da87a4cc
YoussefToumi <yousseftoumi42@gmail.com>
2022-06-22 13:59:20 +0100

    toggling to mysql database

--- src/main/java/com/example/ebankbackend/EbankBackendApplication.java
+++ src/main/java/com/example/ebankbackend/EbankBackendApplication.java
@@ -12,6 +12,7 @@ import com.example.ebankbackend.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
