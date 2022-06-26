32d6663caf48baae9242f0fd7c6a9aa28e9dec9d
YoussefToumi <yousseftoumi42@gmail.com>
2022-06-26 13:50:58 +0100

    adding account display and operations

---
+++ src/main/java/com/example/ebankbackend/dtos/CreditDTO.java
@@ -0,0 +1,10 @@
+package com.example.ebankbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class CreditDTO {
+    private String accountId;
+    private double amount;
+    private String description;
+}
---
+++ src/main/java/com/example/ebankbackend/dtos/TransferRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.ebankbackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class TransferRequestDTO {
+    private String accountSource;
+    private String accountDestination;
+    private double amount;
+    private String description;
+}
